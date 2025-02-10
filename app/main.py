896dcf6517baa0ddc0c31bc203a7d6e31292e993
Sharon Xavier <shaluxmas2@gmail.com>
2025-02-10 22:04:37 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -159,7 +159,7 @@ def add_predictions(input_data):
         st.write("Malignant")
 
 
-    st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
+    st.write("Probability of being Benign: ", model.predict_proba(input_array_scaled)[0][0])
     st.write("Probability of being Malignant: ", model.predict_proba(input_array_scaled)[0][1])
 
     st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
